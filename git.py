38d7fd92b4e86832adf5ed38641e0e51e7deb579
asiasarycheva <assergevna@gmail.com>
2015-10-19 21:29:27 +0300

    remove file git.py

--- git.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-git clone https://github.com/asiasarycheva/asia_homework.git #первый раз клонируем репозиторий в папке появились файлы
-
-git add file.py #добавили файл в локальный репозиторий
-#есть изменение
-git commit -a -m "добавили файл file.py" #сохраняем репозиторий с комментом в локальном 
-git push origin master #отправит ветку master нашего репозитория на сервер origin
